ce3ba0af5cd790deba44449f9d7d035120809d4f
Zachattack98 <bakugan0498@yahoo.com>
2021-04-16 22:48:26 -0700

    Create Tree.h

---
+++ header/Tree.h
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//creating a Depth-First Search Tree containing all possible states until the goal state is reached
+class Tree {
+  public:
+    void DFS_Puzzle(struct Node* node) {
+      if (node == NULL)
+          return;
+ 
+      /* first print data/nxn array of node */
+      cout << node->data << " ";
+ 
+      /* then recur on left subtree */
+      DFS_Puzzle(node->left_child);
+ 
+      /* now recur on right subtree */
+      DFS_Puzzle(node->right_child);
+}
+
