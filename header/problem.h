4610f9087ddb41a1969a52ca20f33bc8787a2852
Zachattack98 <bakugan0498@yahoo.com>
2021-04-16 22:39:12 -0700

    Create problem.h

---
+++ header/problem.h
@@ -0,0 +1,10 @@
+
+#include <bits/stdc++.h>
+#include "Node.h"
+using namespace std;
+
+class Problem {
+  int intial_state;   //the state we begin with before doing any operations
+  int goal_state;     //the anticipated state we hope to end the tree with
+  Node* operators;    //a pointer for all the possible moves each node can make
+};
