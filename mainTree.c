496a672c076bfbe85e10f0bc175117662fdd853b
Zachattack98 <bakugan0498@yahoo.com>
2021-04-16 22:59:07 -0700

    Update mainTree.c

--- mainTree.c
+++ mainTree.c
@@ -1,32 +1,31 @@
 #include <bits/stdc++.h>
+#include "problem.h"
+#include "Node.h"
+#include "Tree.h"
 using namespace std;
  
-struct Node {
-    int data;
-    struct Node* left;
-    struct Node* right;
+int main() {
  
-    // val is the value that has to be added to the data part
-    Node(int val) {
-        data = val;
+    int squares;  //total number of squares (including the blank square) in the puzzle
+    int n;    //variable used for nxn array
  
-        // Left and right child for node
-        // will be initialized to null
-        left = NULL;
-        right = NULL;
+    cout << "How squares are in the puzzle?" << endl;
+    cin >> squares;
+ 
+    if (sqrt(squares) == n) { //check if there is a valid number of squares for an nxn array
+     cout << " Invalid number! Enter again" << endl;
+     cin >> squares;
     }
-};
  
-int main()
-{
- 
-    /*create root*/
+    /*create root; just practice*/
     struct Node* root = new Node(1);
-    
     root->left = new Node(2);
     root->right = new Node(3);
     root->left->left = new Node(4);
-    
+    root->left->right = new Node(5);
+ 
+    cout << "\n" << squares << "-Puzzle Solution of binary tree is \n";
+    DFS_Puzzle(root);
  
     return 0;
 }
