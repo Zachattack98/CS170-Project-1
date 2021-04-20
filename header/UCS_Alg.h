b532c1c01fd5e979631d4e4c4f9456bf991a45a8
Zachattack98 <bakugan0498@yahoo.com>
2021-04-20 03:22:36 -0700

    Create Uniform Cost Search class

---
+++ header/UCS_Alg.h
@@ -0,0 +1,17 @@
+#ifndef UCS_ALG_H_
+#define UCS_ALG_H_
+
+#include <bits/stdc++.h>
+#include "Node.h"
+#include "Tree.h"
+#include "problem.h"
+
+using namespace std;
+
+//creating a Uniforn Cost Search class
+
+class UCS_Alg {
+
+}
+
+#endif
