cc7cc630af2461cb618871aa6f38b905ddc26f12
Zachattack98 <bakugan0498@yahoo.com>
2021-05-02 03:32:12 -0700

    Added valid input loops

--- mainTree.cpp
+++ mainTree.cpp
@@ -8,10 +8,11 @@ using namespace std;
 int puzzlePrompt();
 
 int main() {
-
+    int proceed = 1;
+    
     cout << "Welcome to 862041797 and XXX 8 puzzle solver." << endl;
 
-    while(1) {
+    while(proceed) {
         Node *root;
         int Algorithm;
         int decision;
@@ -19,20 +20,37 @@ int main() {
         cout << "Type 1 to use a default puzzle, or 2 to enter your own puzzle." << endl;
         cin >> decision;
 
+        while (decision != 1 && decision != 2) {
+            cout >> "\nNot a valid response! Please re-enter your choice." << endl;
+            cin >> decision;
+        }
+        
         if (decision == 1) {
             int difficulty;
-            cout << "Choose puzzle Complexity (1-6): ";
+            cout << "\nChoose puzzle Complexity (1-6): ";
             cin >> difficulty;
+            
+            while (difficulty != 1 && difficulty != 2 && difficulty != 3 && difficulty != 4 && difficulty != 5 && difficulty != 6) {
+                cout >> "\nThat is not one of the puzzles! Please re-enter your choice." << endl;
+                cin >> difficulty;
+            }
             root = new Node(difficulty);
+        }
+            
         } else {
             root = new Node('c');
         }
 
-        cout << "Enter your choice of algorithm\n"
+        cout << "\nEnter your choice of algorithm\n"
              << "1. Uniform Cost Search\n2. A* with the Misplaced Tile heuristic.\n3. A* with the Eucledian distance heuristic."
              << endl;
         cin >> Algorithm;
 
+        while (Algorithm != 1 && Algorithm != 2 && Algorithm != 3) {
+            cout >> "\nThat is not one of the algorithms! Please re-enter your choice." << endl;
+            cin >> Algorithm;
+        }
+    
         Problem problem;
         problem.algorithmchoice = Algorithm;
 
@@ -43,6 +61,9 @@ int main() {
             cout << "Solution Failed!" << endl;
             cout << problem.expanded << " nodes expanded." << endl;
         }
+    
+        cout << "\nDo you wish to solve another puzzle? Y or N." << endl;
+        cin >> proceed;
     }
  
     return 0;
